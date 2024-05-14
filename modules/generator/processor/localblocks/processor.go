3e9bde2c3f1b87691cd00b73ba081816dfc54ef2
Mario <mariorvinas@gmail.com>
2024-05-14 10:53:12 +0200

    Add metrics to processor

--- modules/generator/processor/localblocks/processor.go
+++ modules/generator/processor/localblocks/processor.go
@@ -326,6 +326,7 @@ func (p *Processor) completeBlock() error {
 	defer p.blocksMtx.Unlock()
 
 	p.completeBlocks[newMeta.BlockID] = ingester.NewLocalBlock(ctx, newBlock, p.wal.LocalBackend())
+	metricCompletedBlocks.WithLabelValues(p.tenant).Inc()
 
 	err = b.Clear()
 	if err != nil {
@@ -354,6 +355,7 @@ func (p *Processor) flushBlock() error {
 	if err != nil {
 		return err
 	}
+	metricFlushedBlocks.WithLabelValues(p.tenant).Inc()
 	return nil
 }
 
@@ -726,6 +728,7 @@ func (p *Processor) cutBlocks(immediate bool) error {
 	}
 
 	p.walBlocks[p.headBlock.BlockMeta().BlockID] = p.headBlock
+	metricCutBlocks.WithLabelValues(p.tenant).Inc()
 
 	err = p.resetHeadBlock()
 	if err != nil {
